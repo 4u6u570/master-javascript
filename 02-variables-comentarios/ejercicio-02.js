137466aff52a442a76765e46f3d01f7be7fbb380
Augusto <crystalcaverns1991@gmail.com>
2020-04-06 22:26:25 -0300

    Add files via upload

---
+++ 02-variables-comentarios/ejercicio-02.js
@@ -0,0 +1,27 @@
+'user strict'
+
+// Con doble barra se puede comentar para que el navegador  omita toda esta línea de información.
+
+/* Esta es otra forma 
+de hacer comentarios
+pero con múltiples 
+saltos de líneas*/
+
+// VARIABLES
+// Una variable es un contenedor de información.
+
+var pais = "Argentina";
+var continente = "Suramérica";
+var antiguedad = "2020";
+// var pais_y_continente = pais+" "+continente;
+let prueba = "hola";
+alert("hola");
+
+// Reasignando variables
+pais = "México";
+continente = "Latinoamérica";
+var pais_y_continente = pais+" "+continente;
+
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente);
+
