67008361285ab28190990aa0d7e2a2608d2d7429
Augusto <augustosalazar@outlook.com>
2020-05-13 02:25:38 -0300

    Break

---
+++ 11-break/ejercicio-11.js
@@ -0,0 +1,18 @@
+'use strict'
+
+// BUCLE WHILE
+
+var year = 2018;
+
+
+// INCREMENTO ++
+
+while(year != 1991){
+    // Ejecuta esto
+    console.log("Estamos en el año: " + year);
+
+    if(year == 2000){
+        break;
+    }
+    year--;
+}
