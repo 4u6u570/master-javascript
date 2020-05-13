05819986e6b649d6aada8e8fa073b8897c541683
Augusto <augustosalazar@outlook.com>
2020-05-13 03:00:32 -0300

    Ventanas y Alertas

---
+++ 12-ventanas/ejercicio-12.js
@@ -0,0 +1,16 @@
+'use strict'
+
+// ALERTA
+
+//alert("Esta es mi alerta!!");
+//alert("Este es mi texto");
+
+// CONFIRMACIÓN
+
+//var miResultado = confirm("Estás seguro de querer continuar?");
+//console.log(miResultado);
+
+// INGRESO DE DATOS
+
+var miResultado = prompt("¿Qué edad tienes?", 18);
+console.log(typeof miResultado);
