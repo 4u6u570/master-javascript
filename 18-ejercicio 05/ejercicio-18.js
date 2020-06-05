0c7a35a8b5ff7b18edc9adc91a863e66b3b7caa7
Augusto <augustosalazar@outlook.com>
2020-06-04 23:34:12 -0300

    Ejercicio 18
    Usando bucle For y módulo.

---
+++ 18-ejercicio 05/ejercicio-18.js
@@ -0,0 +1,14 @@
+'use strict'
+
+/*
+Muestre todos los números divisores introducidos en un prompt
+*/
+
+var numero = parseInt(prompt("Mete un número", 1));
+
+for (var i = 1; i <= numero; i++) {
+
+    if (numero % i == 0) {
+        console.log("Divisor: " + i);
+    }
+};
