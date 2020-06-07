17a895b3cb7a4c39dffebc65c35f15cfceaf74b5
Augusto <augustosalazar@outlook.com>
2020-06-07 03:21:12 -0300

    Funciones
    Ejercicios de funciones

---
+++ 22-funciones/22-funciones.js
@@ -0,0 +1,17 @@
+'use strict'
+
+// Funciones
+// Una función es una agrupación reutilizable de un conjunto de instrucciones.
+
+function calculadora(){
+    //Conjunto de instrucciones a ejecutar.
+    console.log("Hola soy la calculadora");
+    console.log("Si soy yo");
+    
+    return "Hola soy la calculadora";
+}
+
+//Invocar o llamar a la función.
+var resultado = calculadora();
+
+console.log(resultado);
