4ed19461fb9d95496ecee91ce6b364c3d4bd0177
Augusto <augustosalazar@outlook.com>
2020-08-09 21:21:23 -0300

    Ámbito de las variables

---
+++ 29-ambito-variables/29-ambito-variables.js
@@ -0,0 +1,14 @@
+'use strict'
+
+function  holaMundo(texto) {
+        var hola_mundo = "Texto dentro de función";
+        console.log(texto);
+        console.log(typeof numero.toString());
+        console.log(hola_mundo);
+};
+
+var numero = 12;
+var texto = "Hola mundo soy una variable global";
+holaMundo(texto);
+
+/* console.log(hola_mundo);*/ //No se puede llamar a esta variable ya que está dentro de una función.
