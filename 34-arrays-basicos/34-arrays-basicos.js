d864df52d6ae2bc8218a10b67822de361e491f3a
Augusto <augustosalazar@outlook.com>
2021-05-24 02:23:04 -0300

    Arrays multimendionales

--- 34-arrays-basicos/34-arrays-basicos.js
+++ 34-arrays-basicos/34-arrays-basicos.js
@@ -7,7 +7,7 @@ var nombre = "Augusto Salazar";
 var nombres = ["Augusto Salazar", "Juan López", "Manolo García", "José Martin", 52, true];
 
 var lenguajes = new Array("PHP", "JS", "Go", "Java");
-var elemento = parseInt(prompt("Qué elemento del array quieres??", 0));
+// var elemento = parseInt(prompt("Qué elemento del array quieres??", 0));
 
 /*
 (elemento) > nombres.length 
@@ -17,8 +17,18 @@ var elemento = parseInt(prompt("Qué elemento del array quieres??", 0));
 
 // Recorrer arrays con loops
 
+document.write("<h1>Lenguajes de programación del 2021</h1>");
+document.write("<ul>");
+/*
 for(var i = 0; i < lenguajes.length; i++){
     document.write("<li>"+lenguajes[i]+"</li>");
 }
-
     document.write("</ul>");
+*/
+
+lenguajes.forEach((elementosDelArrayLenguajes, indice)=> {
+    document.write("<li>"+indice+" - "+elementosDelArrayLenguajes+"</li>");
+});
+
+document.write("</ul>");
+
---
+++ 35-arrays-multi/35-arrays-multi.js
@@ -0,0 +1,38 @@
+
+'use strict'
+
+// Arrays Multidimensionales
+
+var categorias = ['Acción', 'Terror', 'Comedia'];
+var peliculas = ['La verdad duele', 'La vida es bella', 'Gran Torino'];
+
+var cine = [categorias, peliculas];
+
+// document.write(cine[0][1]);
+// document.write(cine[1][2]);
+
+/* 
+var elemento = "";
+
+do{
+    elemento = prompt("Introduce tu película");
+    peliculas.push(elemento);
+}while(elemento != "ACABAR");
+
+*/
+
+// peliculas.pop();
+
+var indice = peliculas.indexOf('Gran Torino');
+if(indice > -1){
+    peliculas.splice(indice, 1)
+}
+
+var peliculas_string = peliculas.join();
+
+var cadena = "texto1, texto2, texto3";
+var cadena_array = cadena.split(", ");
+
+console.log(cadena_array);
+//document.write(peliculas_string)
+//document.write(peliculas);
