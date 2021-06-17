9baf86af00799c7fa40521d835b06a9cac06b7ed
Augusto <augustosalazar@outlook.com>
2021-06-17 20:42:31 -0300

    Timers

---
+++ 40-timers/40-timers.js
@@ -0,0 +1,42 @@
+"use strict";
+
+// Eeventos
+
+window.addEventListener("load", () => {
+
+    // Timers
+
+    function intervalo(){
+
+      var tiempo = setInterval(function(){
+        console.log("Set interval ejecutado");
+  
+        var encabezado = document.querySelector("h1");
+  
+        if(encabezado.style.fontSize == "50px"){
+          encabezado.style.fontSize = "30px";
+        }else{
+          encabezado.style.fontSize = "50px";
+        }
+      }, 1000);
+      return tiempo;
+    }
+
+    var tiempo = intervalo();
+
+    var stop = document.querySelector("#stop");
+
+    stop.addEventListener("click", function(){
+      alert("Has parado el intervalo en bucle");
+      clearInterval(tiempo);
+    })
+
+    var start = document.querySelector("#start");
+
+    start.addEventListener("click", function(){
+      alert("Has iniciado el intervalo en bucle");
+      intervalo();
+    })
+});
+
+// el setTimeout solo se ejecuta una vez
